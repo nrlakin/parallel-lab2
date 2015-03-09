5f764aece50383eaacf1bccfbd75501ac350bcdc
Neil Lakin <Neil@dhcp-ccc-9075.redrover.cornell.edu>
2015-03-08 21:19:32 -0400

    ...

--- mpz_test.c
+++ mpz_test.c
@@ -5,32 +5,44 @@
 
 #define TEST_NUM  "123456789123456789123456789123456789"
 
-int serialize_mpz(mpz_t bignum, unsigned char **dest, int *length) {
-  unsigned char *int_count = *dest;
+/*
+int serialize_jobs(struct userdef_job_t **start_job, int n_jobs, unsigned char **array, int *len) {
+  unsigned char *destPtr = *array;
   unsigned char *temp_mpz;
+  struct userdef_job_t **job = start_job;
   size_t mpz_size;
-  int len;
+  int length, i;
+  for(i=0; i<n_jobs; i++) {
+    if(*job == NULL) break;
+
+
+  }
   temp_mpz = mpz_export(NULL, &mpz_size, 1, 1, 1, 0, bignum);
   len = mpz_size;
-  printf("%d\n", (int)mpz_size);
-  *dest = (unsigned char*) malloc(sizeof(unsigned char) * len +1);
-  *length = len;
-  memcpy(*dest, temp_mpz, len);
+  *dest = (unsigned char*) malloc(sizeof(int) + (sizeof(unsigned char) * len)
+                    + 2*sizeof(unsigned long));
+  memcpy(destPtr, &len, sizeof(int));
+  destPtr += 4;
+  memcpy(destPtr, temp_mpz, len);
+  destPtr += len * sizeof(unsigned char);
+
   free(temp_mpz);
   return 0;
-}
+}*/
 
 int main(int argc, char **argv) {
   mpz_t test;
   unsigned char *target;
   int length, i;
   mpz_init_set_str(test, TEST_NUM, 10);
-  serialize_mpz(test, &target, &length);
-  printf("n_bytes: %d\n", length);
-  for(i=0;i<length;i++) {
-    printf("%X\n", target[i]);
-  }
+  //serialize_mpz(test, &target, &length);
+  //printf("n_bytes: %d\n", length);
+  length = (mpz_sizeinbase(test, 2) + 7)/8;
+  printf("test_count: %d\n", length);
+  //for(i=0;i<length;i++) {
+  //  printf("%X\n", target[i]);
+  //}
   mpz_clear(test);
-  free(target);
+  //free(target);
   return 0;
 }

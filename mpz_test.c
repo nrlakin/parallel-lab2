93f3569dec8ca7a440c165bd01fdfdf255765da9
Neil Lakin <Neil@dhcp-ccc-9075.redrover.cornell.edu>
2015-03-08 22:12:17 -0400

    ...

--- mpz_test.c
+++ mpz_test.c
@@ -26,7 +26,6 @@ int serialize_jobs(struct userdef_work_t **start_job, int n_jobs, unsigned char 
   struct userdef_work_t **job = start_job;
   size_t mpz_size;
   int i, mpz_len, length = 0;
-  printf("made it to serializer\n");
   for(i=0; i<n_jobs; i++) {
     if(*job == NULL) break;
     length += sizeof(int);    //for mpz size
@@ -34,29 +33,23 @@ int serialize_jobs(struct userdef_work_t **start_job, int n_jobs, unsigned char 
     length += 2*sizeof(unsigned long);  //for start/end
     job++;
   }
-  printf("serializer thinks length is %d\n", length);
   if (NULL == (*array = (unsigned char*)malloc(sizeof(unsigned char) * length))) {
     printf("malloc failed on allocating send buffer\n");
     return 0;
   }
-  printf("past malloc\n");
   job = start_job;
   destPtr = *array;
   *len = length;
   for(i=0; i<n_jobs; i++) {
     if(*job == NULL)break;
     mpz_len = get_mpz_length((*job)->target);
-    printf("mpz_len: %d\n", mpz_len);
     memcpy(destPtr, &mpz_len, sizeof(int));
     destPtr += sizeof(int);
-    printf("before mpz_export\n");
     mpz_export(destPtr, &mpz_size, ORDER, WORD_SIZE, ENDIAN, NAIL, (*job)->target);
-    printf("after mpz_export\n");
     if (mpz_len != mpz_size) {
       printf("mpz error--different functions report different sizes.\n");
       return 0;
     }
-    printf("serializer length: %d\n", mpz_len);
     destPtr += mpz_len;
     memcpy(destPtr, &((*job)->rangeStart), sizeof(unsigned long));
     destPtr += sizeof(unsigned long);
@@ -67,11 +60,41 @@ int serialize_jobs(struct userdef_work_t **start_job, int n_jobs, unsigned char 
   return 1;
 }
 
+int deserialize_jobs(struct userdef_work_t **queue, unsigned char *array, int len) {
+  struct userdef_work_t *jobPtr;
+  unsigned char *srcPtr = array;
+  size_t mpz_size;
+  int temp_size;
+  while (NULL != *queue)queue++;
+  while(len) {
+    if (NULL == (jobPtr = (struct userdef_work_t*)malloc(sizeof(struct userdef_work_t)))) {
+      printf ("malloc failed on receive buffer...\n");
+      return 0;
+    };
+    memcpy(&temp_size, srcPtr, sizeof(int));
+    mpz_size = temp_size;
+    srcPtr += sizeof(int);
+    mpz_import(jobPtr->target, mpz_size, ORDER, WORD_SIZE, ENDIAN, NAIL, srcPtr);
+    srcPtr += temp_size;
+    memcpy(&(resultPtr->rangeStart), srcPtr, sizeof(sizeof(unsigned long)));
+    srcPtr += sizeof(unsigned long);
+    memcpy(&(resultPtr->rangeEnd), srcPtr, sizeof(sizeof(unsigned long)));
+    srcPtr += sizeof(unsigned long);
+    *queue++ = resultPtr;
+    len-= sizeof(int) + temp_size + 2*sizeof(unsigned long);
+  }
+  *queue = NULL;
+  return 1;
+}
+
 int main(int argc, char **argv) {
   struct userdef_work_t test;
   struct userdef_work_t *work_queue[2];
+  struct userdef_work_t *rec_work[2];
   unsigned char *target;
   int length, i;
+  rec_work[0] = NULL;
+
   mpz_init_set_str(test.target, TEST_NUM, 10);
   test.rangeStart = 1;
   test.rangeEnd = 5;
@@ -85,10 +108,17 @@ int main(int argc, char **argv) {
   for(i=0; i<length; i++) {
     printf("%X\n", target[i]);
   }
+  deserialize_jobs(rec_work, target, length);
+  printf("received target: ");
+  mpz_out_str(stdout, 10, rec_work[0]->target);
+  printf("\nrangeStart: %ld\n", rec_work[0]->rangeStart);
+  printf("rangeEnd: %ld\n", rec_work[0]->rangeEnd);
   //for(i=0;i<length;i++) {
   //  printf("%X\n", target[i]);
   //}
   mpz_clear(test.target);
   free(target);
+  mpz_clear(rec_work[0]->target);
+  free(rec_work[0]);
   return 0;
 }
